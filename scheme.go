60251566b27e9c9aea793ae30528a63269f728c8
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2021-05-09 21:32:40 +0900

    query

--- scheme.go
+++ scheme.go
@@ -16,12 +16,24 @@ package main
 // var createRecordDatas = `
 // 	create table record_datas(
 // 		id bigint not null auto_increment primary key,
-// 		FOREIGN KEY(user_id) REFERENCES uesrs (id),
+// 		FOREIGN KEY(user_id) REFERENCES users (id),
 // 		file_name varchar(20) not null,
 // 		record_date date not null,
 // 		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 // 		updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 // 		)`
+
+// var createDiary = `create table diary(
+// 	id bigint not null auto_increment primary key,
+// 	user_id bigint not null,
+// 	FOREIGN KEY(user_id) REFERENCES users (id),
+// 	emotional_state int not null,
+// 	physical_condition int not null,
+// 	texts varchar(1000) not null,
+// 	diary_date date not null,
+// 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+// 	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+// 	)`
 
 // var insertUser = `
 // 	insert into users (name, email, age, password, baby_name, baby_nickname, baby_birth)
@@ -29,6 +41,11 @@ package main
 // 	`
 
 // var insertRecordData = `
-// 	insert into record_datas (uesr_id, file_name, record_date)
+// 	insert into record_datas (user_id, file_name, record_date)
 // 	values (1, "test1", "2020-01-01")
 // 	`
+
+// var insertDiary = `
+// 	insert into diary (user_id, emotional_state, physical_condition, texts, diary_date)
+// 	values (1, 1, 1, "테스트 내용입니다.", "2020-01-01")
+// `
